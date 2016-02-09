fba941f8bea21efc59fbcb08872bcbec6dbb84d4
Sebastian Porto <sebasporto@gmail.com>
2016-02-09 22:42:19 +1100

    More comments

--- src/Players/Utils.elm
+++ src/Players/Utils.elm
@@ -5,10 +5,21 @@ import Perks.Models exposing (Perk)
 import PerksPlayers.Models exposing (PerkPlayer)
 
 
+{-
+Return total bonuses for a player
+-}
+
+
 bonusesForPlayerId : List PerkPlayer -> List Perk -> PlayerId -> Int
 bonusesForPlayerId perksPlayers perks playerId =
   perksForPlayerId perksPlayers perks playerId
     |> List.foldl (\perk acc -> acc + perk.bonus) 0
+
+
+
+{-
+Return list of perks for a player
+-}
 
 
 perksForPlayerId : List PerkPlayer -> List Perk -> PlayerId -> List Perk
@@ -19,6 +30,15 @@ perksForPlayerId perksPlayers perks playerId =
   in
     perks
       |> List.filter (\perk -> List.any (\id -> id == perk.id) perkIds)
+
+
+
+{-
+Given
+  - List of perksPlayers
+  - A player
+Return list of perk ids for a player
+-}
 
 
 perkIdsForPlayerId : List PerkPlayer -> PlayerId -> List Int
