369585439bf73a5e2ac21de764dd10a88d3518ed
Sebastian Porto <sebasporto@gmail.com>
2016-02-10 17:20:38 +1100

    Uncomment css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-// require('basscss/css/basscss.css');
-// require('font-awesome/css/font-awesome.css');
+require('basscss/css/basscss.css');
+require('font-awesome/css/font-awesome.css');
 
 // Require index.html so it gets copied to dist
 require('./index.html');
