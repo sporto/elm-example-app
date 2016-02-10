4493d1a9076f60b0a40ffbe2319f2d254d72950d
Sebastian Porto <sebasporto@gmail.com>
2016-02-11 08:56:39 +1100

    Broken player edit

--- src/Players/Edit.elm
+++ src/Players/Edit.elm
@@ -23,19 +23,14 @@ view address model =
 nav : Signal.Address Action -> ViewModel -> Html.Html
 nav address model =
   div
-    [ class "bg-silve" ]
-    [ a [ class "btn button-narrow", href "#/players" ] [ text "Players" ]
-    , span [] [ text " > " ]
-    , span [ class "btn button-narrow" ] [ text model.player.name ]
+    []
+    [ a [ class "btn button-narrow", href "#/players" ] [ text "< Players" ]
     ]
 
 
 form : Signal.Address Action -> ViewModel -> Html.Html
 form address model =
   let
-    player =
-      model.player
-
     bonuses =
       999
 
@@ -45,50 +40,90 @@ form address model =
     div
       [ class "m2" ]
       [ h1 [] [ text model.player.name ]
-      , div
-          [ class "clearfix py1"
-          ]
-          [ div [ class "col col-5" ] [ text "Level" ]
-          , div
-              [ class "col col-7" ]
-              [ span [ class "h2 bold" ] [ text (toString player.level) ]
-              , a
-                  [ class "btn ml1 h1" ]
-                  [ i [ class "fa fa-minus-circle" ] [] ]
-              , a
-                  [ class "btn ml1 h1" ]
-                  [ i [ class "fa fa-plus-circle" ] [] ]
-              ]
-          ]
-      , div
-          [ class "clearfix py1" ]
-          [ div [ class "col col-5" ] [ text "Bonuses" ]
-          , div [ class "col col-7 h2" ] [ text (toString bonuses) ]
-          ]
-      , div
-          [ class "clearfix py1" ]
-          [ div [ class "col col-5" ] [ text "Strength" ]
-          , div [ class "col col-7 h2 bold" ] [ text (toString strength) ]
-          ]
-      , div
-          [ class "clearfix py1"
-          ]
-          [ div [ class "col col-5" ] [ text "Name" ]
-          , div
-              [ class "col col-7" ]
-              [ input
-                  [ class "field-light"
-                  , value player.name
-                  ]
-                  []
-              ]
-          ]
-      , div
-          [ class "clearfix py1"
-          ]
-          [ div [ class "col col-5" ] [ text "Perks" ]
-          , div
-              [ class "col col-7" ]
-              []
-          ]
+      , formLevel address model
+      , formBonuses bonuses
+      , formStrength strength
+      , formName address model
+      , formPerks address model
       ]
+
+
+formLevel : Signal.Address Action -> ViewModel -> Html.Html
+formLevel address model =
+  div
+    [ class "clearfix py1"
+    ]
+    [ div [ class "col col-5" ] [ text "Level" ]
+    , div
+        [ class "col col-7" ]
+        [ span [ class "h2 bold" ] [ text (toString model.player.level) ]
+        , btnLevelDecrease address model
+        , btnLevelIncrease address model
+        ]
+    ]
+
+
+btnLevelDecrease : Signal.Address Action -> ViewModel -> Html.Html
+btnLevelDecrease address model =
+  a
+    [ class "btn ml1 h1" ]
+    [ i [ class "fa fa-minus-circle" ] [] ]
+
+
+btnLevelIncrease : Signal.Address Action -> ViewModel -> Html.Html
+btnLevelIncrease address model =
+  a
+    [ class "btn ml1 h1" ]
+    [ i [ class "fa fa-plus-circle" ] [] ]
+
+
+formBonuses : Int -> Html.Html
+formBonuses bonuses =
+  div
+    [ class "clearfix py1" ]
+    [ div [ class "col col-5" ] [ text "Bonuses" ]
+    , div [ class "col col-7 h2" ] [ text (toString bonuses) ]
+    ]
+
+
+formStrength : Int -> Html.Html
+formStrength strength =
+  div
+    [ class "clearfix py1" ]
+    [ div [ class "col col-5" ] [ text "Strength" ]
+    , div [ class "col col-7 h2 bold" ] [ text (toString strength) ]
+    ]
+
+
+formName : Signal.Address Action -> ViewModel -> Html.Html
+formName address model =
+  div
+    [ class "clearfix py1"
+    ]
+    [ div [ class "col col-5" ] [ text "Name" ]
+    , div
+        [ class "col col-7" ]
+        [ inputName address model
+        ]
+    ]
+
+
+inputName : Signal.Address Action -> ViewModel -> Html.Html
+inputName address model =
+  input
+    [ class "field-light"
+    , value model.player.name
+    ]
+    []
+
+
+formPerks : Signal.Address Action -> ViewModel -> Html.Html
+formPerks address model =
+  div
+    [ class "clearfix py1"
+    ]
+    [ div [ class "col col-5" ] [ text "Perks" ]
+    , div
+        [ class "col col-7" ]
+        []
+    ]
