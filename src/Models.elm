140efc6ca67bf20012f833a354937187515bdf57
Sebastian Porto <sebasporto@gmail.com>
2016-02-10 18:03:21 +1100

    150 Added routing to main model

--- src/Models.elm
+++ src/Models.elm
@@ -1,14 +1,17 @@
 module Models (..) where
 
 import Players.Models exposing (Player)
+import Routing
 
 
 type alias AppModel =
   { players : List Player
+  , routing : Routing.Model
   }
 
 
 initialModel : AppModel
 initialModel =
   { players = [ Player 1 "Sam" 1 ]
+  , routing = Routing.initialModel
   }
