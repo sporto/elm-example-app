a0dc8b54ad10c27d752a3ad00511ec7c1d20c19e
Sebastian Porto <sebasporto@gmail.com>
2016-02-10 18:16:20 +1100

    140 Added players update

---
+++ src/Players/Update.elm
@@ -0,0 +1,15 @@
+module Players.Update (..) where
+
+import Effects exposing (Effects)
+import Players.Actions exposing (..)
+import Players.Models exposing (..)
+
+
+type alias UpdateModel =
+  { players : List Player
+  }
+
+
+update : Action -> UpdateModel -> ( List Player, Effects Action )
+update action model =
+  ( model.players, Effects.none )
--- src/Update.elm
+++ src/Update.elm
@@ -1,10 +1,24 @@
 module Update (..) where
 
+import Effects exposing (Effects)
 import Models exposing (..)
 import Actions exposing (..)
-import Effects exposing (Effects)
+import Players.Update
 
 
 update : Action -> AppModel -> ( AppModel, Effects Action )
 update action model =
-  ( model, Effects.none )
+  case action of
+    PlayersAction subAction ->
+      let
+        updateModel =
+          { players = model.players
+          }
+
+        ( updatedPlayers, fx ) =
+          Players.Update.update subAction updateModel
+      in
+        ( { model | players = updatedPlayers }, Effects.map PlayersAction fx )
+
+    NoOp ->
+      ( model, Effects.none )
