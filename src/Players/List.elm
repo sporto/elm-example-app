18808471ad56dfab03d671208ed9e73a36d38548
Sebastian Porto <sebasporto@gmail.com>
2016-02-11 19:19:00 +1100

    Added nav to players

--- src/Players/List.elm
+++ src/Players/List.elm
@@ -15,10 +15,23 @@ view : Signal.Address Action -> ViewModel -> Html.Html
 view address model =
   div
     []
-    [ div
-        [ class "right-align" ]
-        []
-    , table
+    [ nav address model
+    , list address model
+    ]
+
+
+nav : Signal.Address Action -> ViewModel -> Html.Html
+nav address model =
+  div
+    [ class "clearfix mb2 white bg-black" ]
+    [ div [ class "left p2" ] [ text "Players" ] ]
+
+
+list : Signal.Address Action -> ViewModel -> Html.Html
+list address model =
+  div
+    []
+    [ table
         [ class "table-light" ]
         [ thead
             []
