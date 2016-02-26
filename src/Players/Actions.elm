6f037eed2875e97e876e9726ec7334f428e13ad5
Sebastian Porto <sebasporto@gmail.com>
2016-02-26 20:25:25 +1100

    Removed actions that don't belong

--- src/Players/Actions.elm
+++ src/Players/Actions.elm
@@ -1,9 +1,7 @@
 module Players.Actions (..) where
 
-import Hop
 import Players.Models exposing (PlayerId, Player)
 
 
 type Action
   = NoOp
-  | HopAction Hop.Action
--- src/Players/Update.elm
+++ src/Players/Update.elm
@@ -1,6 +1,5 @@
 module Players.Update (..) where
 
-import Hop
 import Effects exposing (Effects)
 import Players.Actions exposing (..)
 import Players.Models exposing (..)
@@ -13,9 +12,4 @@ type alias UpdateModel =
 
 update : Action -> UpdateModel -> ( List Player, Effects Action )
 update action model =
-  case action of
-    HopAction payload ->
-      ( model.players, Effects.none )
-
-    NoOp ->
-      ( model.players, Effects.none )
+  ( model.players, Effects.none )
