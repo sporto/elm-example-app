eb1d3f6feab91dc205eb5247c2a949cf6424e269
Sebastian Porto <sebasporto@gmail.com>
2016-06-05 14:20:52 +1000

    Reformat code

--- src/Main.elm
+++ src/Main.elm
@@ -5,23 +5,28 @@ import Messages exposing (Msg(..))
 import Models exposing (Model, initialModel)
 import View exposing (view)
 import Update exposing (update)
-import Players.Commands exposing(fetchAll)
+import Players.Commands exposing (fetchAll)
 
-init : (Model, Cmd Msg)
+
+init : ( Model, Cmd Msg )
 init =
-  ( initialModel, Cmd.map PlayersMsg fetchAll )
+    ( initialModel, Cmd.map PlayersMsg fetchAll )
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.none
+
+
 
 -- MAIN
 
-main =
-  Html.App.program
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
 
+main : Program Never
+main =
+    Html.App.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
--- src/Models.elm
+++ src/Models.elm
@@ -2,11 +2,13 @@ module Models exposing (..)
 
 import Players.Models exposing (Player)
 
+
 type alias Model =
-  { players : List Player
-  }
+    { players : List Player
+    }
+
 
 initialModel : Model
 initialModel =
-  { players = [ ]
-  }
+    { players = []
+    }
