0db339ba93b92d98589386b64ceddddc5c3c6aac
Sebastian Porto <sebasporto@gmail.com>
2016-06-05 14:02:57 +1000

    Run elm-format

--- src/Main.elm
+++ src/Main.elm
@@ -3,46 +3,66 @@ module Main exposing (..)
 import Html exposing (Html, div, text)
 import Html.App
 
+
 -- MODEL
 
-type alias Model = String
 
-init : (Model, Cmd Msg)
+type alias Model =
+    String
+
+
+init : ( Model, Cmd Msg )
 init =
-  ("Hello" , Cmd.none)
-  
+    ( "Hello", Cmd.none )
+
+
+
 -- MESSAGES
 
+
 type Msg
-  = NoOp
+    = NoOp
+
+
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-  div []
-    [ text model ]
+    div []
+        [ text model ]
+
+
 
 -- UPDATE
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    NoOp ->
-      (model, Cmd.none)
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
 
 -- SUBSCRIPTIONS
 
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.none
+
+
 
 -- MAIN
 
+
+main : Program Never
 main =
-  Html.App.program
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    Html.App.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
