7db5ae667c10bb3939203bbf557273a2bda1fba9
Sebastian Porto <s@porto5.com>
2018-08-24 18:04:44 +1000

    Add FontAwesome

--- src/Pages/List.elm
+++ src/Pages/List.elm
@@ -2,7 +2,7 @@ module Pages.List exposing (view)
 
 import Html exposing (..)
 import Html.Attributes exposing (class, href)
-import Routes exposing (playerPath)
+import Routes
 import Shared exposing (..)
 
 
@@ -12,10 +12,10 @@ view players =
         [ table []
             [ thead []
                 [ tr []
-                    [ th [] [ text "Id" ]
-                    , th [] [ text "Name" ]
-                    , th [] [ text "Level" ]
-                    , th [] [ text "Actions" ]
+                    [ th [ class "p-2" ] [ text "Id" ]
+                    , th [ class "p-2" ] [ text "Name" ]
+                    , th [ class "p-2" ] [ text "Level" ]
+                    , th [ class "p-2" ] [ text "Actions" ]
                     ]
                 ]
             , tbody [] (List.map playerRow players)
@@ -26,10 +26,10 @@ view players =
 playerRow : Player -> Html Msg
 playerRow player =
     tr []
-        [ td [] [ text player.id ]
-        , td [] [ text player.name ]
-        , td [] [ text (String.fromInt player.level) ]
-        , td []
+        [ td [ class "p-2" ] [ text player.id ]
+        , td [ class "p-2" ] [ text player.name ]
+        , td [ class "p-2" ] [ text (String.fromInt player.level) ]
+        , td [ class "p-2" ]
             [ editBtn player ]
         ]
 
@@ -38,10 +38,10 @@ editBtn : Player -> Html.Html Msg
 editBtn player =
     let
         path =
-            playerPath player.id
+            Routes.playerPath player.id
     in
     a
         [ class "btn regular"
         , href path
         ]
-        [ i [ class "fa fa-pencil mr-1" ] [], text "Edit" ]
+        [ i [ class "fa fa-edit mr-1" ] [], text "Edit" ]
