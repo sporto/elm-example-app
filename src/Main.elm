40f8acf55de9cb1f4265c6ebe72c9b67f2e0f22f
Sebastian Porto <s@porto5.com>
2018-12-10 20:00:49 +1100

    Remove underscores

--- src/Main.elm
+++ src/Main.elm
@@ -20,16 +20,16 @@ type alias Model =
 
 
 type Page
-    = Page_None
-    | Page_List List.Model
-    | Page_Edit Edit.Model
+    = PageNone
+    | PageList List.Model
+    | PageEdit Edit.Model
 
 
 type Msg
     = OnUrlChange Url
     | OnUrlRequest UrlRequest
-    | Msg_List List.Msg
-    | Msg_Edit Edit.Msg
+    | ListMsg List.Msg
+    | EditMsg Edit.Msg
 
 
 init : Flags -> Url -> Key -> ( Model, Cmd Msg )
@@ -39,7 +39,7 @@ init flags url navKey =
             { flags = flags
             , navKey = navKey
             , route = Routes.parseUrl url
-            , page = Page_None
+            , page = PageNone
             }
     in
     ( model, Cmd.none )
@@ -56,17 +56,17 @@ loadCurrentPage ( model, cmd ) =
                         ( pageModel, pageCmd ) =
                             List.init model.flags
                     in
-                    ( Page_List pageModel, Cmd.map Msg_List pageCmd )
+                    ( PageList pageModel, Cmd.map ListMsg pageCmd )
 
                 Routes.PlayerRoute playerId ->
                     let
                         ( pageModel, pageCmd ) =
                             Edit.init model.flags playerId
                     in
-                    ( Page_Edit pageModel, Cmd.map Msg_Edit pageCmd )
+                    ( PageEdit pageModel, Cmd.map EditMsg pageCmd )
 
                 Routes.NotFoundRoute ->
-                    ( Page_None, Cmd.none )
+                    ( PageNone, Cmd.none )
     in
     ( { model | page = page }, Cmd.batch [ cmd, newCmd ] )
 
@@ -74,13 +74,13 @@ loadCurrentPage ( model, cmd ) =
 subscriptions : Model -> Sub Msg
 subscriptions model =
     case model.page of
-        Page_List pageModel ->
-            Sub.map Msg_List (List.subscriptions pageModel)
+        PageList pageModel ->
+            Sub.map ListMsg (List.subscriptions pageModel)
 
-        Page_Edit pageModel ->
-            Sub.map Msg_Edit (Edit.subscriptions pageModel)
+        PageEdit pageModel ->
+            Sub.map EditMsg (Edit.subscriptions pageModel)
 
-        Page_None ->
+        PageNone ->
             Sub.none
 
 
@@ -107,28 +107,28 @@ update msg model =
             ( { model | route = newRoute }, Cmd.none )
                 |> loadCurrentPage
 
-        ( Msg_List subMsg, Page_List pageModel ) ->
+        ( ListMsg subMsg, PageList pageModel ) ->
             let
                 ( newPageModel, newCmd ) =
                     List.update subMsg pageModel
             in
-            ( { model | page = Page_List newPageModel }
-            , Cmd.map Msg_List newCmd
+            ( { model | page = PageList newPageModel }
+            , Cmd.map ListMsg newCmd
             )
 
-        ( Msg_List subMsg, _ ) ->
+        ( ListMsg subMsg, _ ) ->
             ( model, Cmd.none )
 
-        ( Msg_Edit subMsg, Page_Edit pageModel ) ->
+        ( EditMsg subMsg, PageEdit pageModel ) ->
             let
                 ( newPageModel, newCmd ) =
                     Edit.update model.flags subMsg pageModel
             in
-            ( { model | page = Page_Edit newPageModel }
-            , Cmd.map Msg_Edit newCmd
+            ( { model | page = PageEdit newPageModel }
+            , Cmd.map EditMsg newCmd
             )
 
-        ( Msg_Edit subMsg, _ ) ->
+        ( EditMsg subMsg, _ ) ->
             ( model, Cmd.none )
 
 
@@ -160,15 +160,15 @@ currentPage model =
     let
         page =
             case model.page of
-                Page_List pageModel ->
+                PageList pageModel ->
                     List.view pageModel
-                        |> Html.map Msg_List
+                        |> Html.map ListMsg
 
-                Page_Edit pageModel ->
+                PageEdit pageModel ->
                     Edit.view pageModel
-                        |> Html.map Msg_Edit
+                        |> Html.map EditMsg
 
-                Page_None ->
+                PageNone ->
                     notFoundView
     in
     section []
